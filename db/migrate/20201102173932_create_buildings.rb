0a90fc84a617c6a44bf4a213d2e94a9a7b109f34
fozia@iuj.ac.jp <fozia@iuj.ac.jp>
2020-11-02 17:49:03 +0000

    migrated all my files

--- db/migrate/20201102173932_create_buildings.rb
+++ db/migrate/20201102173932_create_buildings.rb
@@ -1,6 +1,11 @@
 class CreateBuildings < ActiveRecord::Migration[6.0]
   def change
     create_table :buildings do |t|
+      t.string :building
+      t.string :rent
+      t.string :address
+      t.string :building_age
+      t.string :feedbacks
 
       t.timestamps
     end
--- db/migrate/20201102174208_create_stations.rb
+++ db/migrate/20201102174208_create_stations.rb
@@ -1,6 +1,9 @@
 class CreateStations < ActiveRecord::Migration[6.0]
   def change
     create_table :stations do |t|
+      t.string :rail_way_line
+      t.string :station_name
+      t.references :building, foreign_key: true
 
       t.timestamps
     end
