5fa0d8d2a9a28fc437367dedcadccf6c61c8b068
fozia@iuj.ac.jp <fozia@iuj.ac.jp>
2020-11-02 17:42:36 +0000

    created station model

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,2 +1,18 @@
 class Building < ApplicationRecord
+  has_many :stations
+  validates :building, presence: true
+  validates :rent, presence: true
+  validates :address, presence: true
+  validates :building_age, presence: true
+  validates :rent, numericality:true
+
+  accepts_nested_attributes_for :stations, reject_if: :all_blank, :allow_destroy => true
+
+  def check
+    if minutes_walk.present? && minutes_walk1.present?
+      if minutes_walk <= minutes_walk1
+        errors.add(:minutes_walk, 'Station one is further than station two')
+      end
+    end
+  end
 end
